b4f329eb85f5e86d3822e4e11f2932e8c71bffb0
maximus-lynn <max.r.k.lynn@gmail.com>
2020-12-04 17:12:28 +0000

    pretty sure the foundation mixin from the previous build was causing global styling not to work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,61 +1,21 @@
 import { Router } from 'next/router';
 import { useState } from 'react';
+import { createGlobalStyle } from 'styled-components';
+import breakpoints from '../assets/css/breakpoints';
 
 // CSS
 // import GlobalCSS from '../assets/css/global';
 import '../assets/css/variables.css';
 
-const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-  }
-
-  html {
-    font-size: 85%;
-
-    @include breakpoint(medium) {
-        font-size: 86%;
-    }
-
-    @include breakpoint(large) {
-        font-size: 90%;
-    }
-  }
-
-  body {
-    font-family: var(--body-font-family);
-    color: var(--body-font-color);
-    margin: 0;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    font-family: var(--header-font-family);
-    text-transform: uppercase;
-    letter-spacing: 6px;
-  }
-
-  p {
-    line-height: 1.5;
-    letter-spacing: 2px;
-  }
-
-  a {
-    display: inline-block;
-    color: var(--white);
-    font-weight: bold;
-
-    cursor: pointer;
-  }
-`;
-
 // Atoms
 import Loading from '../components/atoms/Loading';
-import { createGlobalStyle } from 'styled-components';
+
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    display: none;
+  }
+`;
 
 function App({ Component, pageProps }) {
   const [loading, setLoading] = useState<boolean>(false);
